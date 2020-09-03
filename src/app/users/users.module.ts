93aae61c5bbc5a87c41d2949c7e5c7210ca8598c
chahlaouy <chahlaouy1991@gmail.com>
2020-09-03 15:47:57 +0100

    ADD Home page

---
+++ src/app/users/users.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { LoginComponent } from '../users/login/login.component';
+import { RegisterComponent } from '../users/register/register.component'
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+];
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
+})
+export class UsersModule { }
